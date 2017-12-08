34a17b31af4a97c2d1db3d097e42c3c4feca20c8
William Wong <compulim@hotmail.com>
2017-12-08 11:12:17 -0800

    Move to create-react-app

---
+++ src/containers/mainContent/project.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Experience from './experience';
+
+export default class Project extends React.Component {
+  render() {
+    let { subTitle } = this.props;
+
+    if (/^https?:\/\//.test(subTitle)) {
+      subTitle = React.createElement('a', { href: subTitle, target: '_blank' }, subTitle );
+    }
+
+    return (
+      <Experience
+        from        ={ this.props.from }
+        to          ={ this.props.to }
+        organization={ this.props.title }
+        post        ={ subTitle }
+        verticalDate={ true }
+      >
+        { this.props.children }
+      </Experience>
+    );
+  }
+};
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+const BODY_FONT_FAMILY = `Calibri, 'Helvetica Neue', Arial, sans-serif`;
+const TITLE_FONT_FAMILY = `Montserrat, Arial, sans-serif`;
+// const TITLE_FONT_FAMILY = `Muli, Arial, sans-serif`;
+const SECTION_TITLE_FONT_FAMILY = `Montserrat, Arial, sans-serif`;
+
+export {
+  BODY_FONT_FAMILY,
+  SECTION_TITLE_FONT_FAMILY,
+  TITLE_FONT_FAMILY
+};
