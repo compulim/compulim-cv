fbdff478fa3254bdef2763171cd85371b80f1025
William Wong <compulim@hotmail.com>
2018-03-17 16:31:22 -0700

    Write to file

--- scripts/dockerTag.js
+++ scripts/dockerTag.js
@@ -1,4 +1,5 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
+const { basename, extname } = require('path');
 
 const DOCKER_TAG_PATTERN = /(compulim.azurecr.io\/.*?):[\d\w\.-]+/g;
 
@@ -13,9 +14,15 @@ function promisify(fn, context) {
 }
 
 async function main() {
-  const content = await promisify(readFile)(process.argv[2], { encoding: 'utf8' });
+  const filename = process.argv[2];
+  const content = await promisify(readFile)(filename, { encoding: 'utf8' });
+  const nextContent = content.replace(DOCKER_TAG_PATTERN, `$1:${ process.argv[3] }`);
+  const ext = extname(filename);
+  const nextFilename = `${ filename.substr(0, filename.length - ext.length) }-tagged${ ext }`;
 
-  console.log(content.replace(DOCKER_TAG_PATTERN, `$1:${ process.argv[3] }`));
+  await promisify(writeFile)(nextFilename, nextContent);
+
+  console.log(`Deployment file written to ${ nextFilename }`);
 }
 
 main().catch(err => {
