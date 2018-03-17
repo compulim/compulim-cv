5dcd1e596d269b21f302d01d4652187e212e474d
William Wong <compulim@hotmail.com>
2018-03-17 04:00:18 -0700

    Add PixelDuet.com

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,12 +1,21 @@
 import http from 'http';
 import httpProxy from 'http-proxy';
 
+const DEFAULT_HOST_PORT = '4001';
+
+const HOST_PORT = {
+  'compulim.info': '4001',
+  'www.compulim.info': '4001',
+  'pixelduet.com': '4002',
+  'www.pixelduet.com': '4002'
+};
+
 export default function server() {
   const proxy = httpProxy.createProxyServer({});
 
   const app = http.createServer((req, res) => {
     proxy.web(req, res, {
-      target: 'http://localhost:4001',
+      target: `http://localhost:${ HOST_PORT[req.headers.host] || DEFAULT_HOST_PORT }`,
       ws: true
     });
   });
