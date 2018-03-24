cd46b98ebcebe35bd1b8b470e28b065877db142f
William Wong <compulim@hotmail.com>
2018-03-24 14:45:25 -0700

    Add health check

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,8 +1,6 @@
 import http from 'http';
 import httpProxy from 'http-proxy';
 
-const DEFAULT_HOST_PORT = '4001';
-
 const HOST_PORT = {
   'compulim.info': '4001',
   'www.compulim.info': '4001',
@@ -14,10 +12,20 @@ export default function server() {
   const proxy = httpProxy.createProxyServer({});
 
   const app = http.createServer((req, res) => {
-    proxy.web(req, res, {
-      target: `http://localhost:${ HOST_PORT[req.headers.host] || DEFAULT_HOST_PORT }`,
-      ws: true
-    });
+    const targetPort = HOST_PORT[req.headers.host];
+
+    if (targetPort) {
+      proxy.web(req, res, {
+        target: `http://localhost:${ targetPort }`,
+        ws: true
+      });
+    } else if (req.url === '/health.txt') {
+      res.statusCode = 200;
+      res.end('OK');
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
   });
 
   app.listen(process.env.PORT || 4000);
